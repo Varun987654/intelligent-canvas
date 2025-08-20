a6402f8ace5960ee90f3c91dc675b7c86bd8972b
Varun Reddy Guthikonda <varunreddyguthikonda@gmail.com>
2025-08-21 02:19:44 +0530

    Update socket client to use environment variable for URL

--- lib/socket.ts
+++ lib/socket.ts
@@ -2,9 +2,11 @@ import { io, Socket } from 'socket.io-client'
 
 let socket: Socket | null = null
 
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:3001'
+
 export const getSocket = () => {
   if (!socket) {
-    socket = io('http://localhost:3001', {
+    socket = io(SOCKET_URL, {
       autoConnect: false,
       reconnection: true,
       reconnectionDelay: 1000,
